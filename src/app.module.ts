9d4a3a3a4ce02b59b13b41da7f403164f2ebd3f1
Rodrigo Junqueira <rodrigojunqueira9@gmail.com>
2025-03-13 12:49:07 -0300

    Correções no projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PacientModule } from './pacient/pacient.module';
 
-@Module({
-  imports: [PacientModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
+@Module({})
 export class AppModule {}
