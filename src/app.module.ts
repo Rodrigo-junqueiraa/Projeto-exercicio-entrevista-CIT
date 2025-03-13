1726d2a9d9909b972f58c4007477269d4085ce4f
Rodrigo Junqueira <rodrigojunqueira9@gmail.com>
2025-03-13 11:27:57 -0300

    Adicionando modulo de paciente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PacientModule } from './pacient/pacient.module';
 
 @Module({
-  imports: [],
+  imports: [PacientModule],
   controllers: [AppController],
   providers: [AppService],
 })
